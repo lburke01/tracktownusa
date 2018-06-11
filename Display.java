4df5625304760232c17c5b906573edb52ae0de7a
lburke01 <lburkerobotics@gmail.com>
2018-06-11 05:02:25 -0400

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.RadioButton;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -12,29 +14,34 @@ public class Display extends JFrame
 {
     ButtonGroup groupAt = new ButtonGroup();
     ButtonGroup groupAb = new ButtonGroup();
+    Player active;
+    Environment actEnv;
     public static JMenuBar atBar = new JMenuBar();
     public static JMenuBar abBar = new JMenuBar();
-    public static JMenuBar itBar = new JMenuBar();
     public static JMenuBar tarBar = new JMenuBar();
     public static JMenuBar allBar = new JMenuBar();
     JMenu atMenu = new JMenu("Attack");
     JMenu abMenu = new JMenu("Ability casting");
-    JMenu itMenu = new JMenu("Item usage");
     JMenu tarMenu = new JMenu("Targeting");
     JMenu allMenu = new JMenu("All targeting");
-    JFrame screen = new JFrame("Fight Game");
-    Canvas panel = new Canvas();
+    JFrame screen;
+    Canvas panel;
     JButton attack = new JButton("Attack");
     JButton useAb = new JButton("Use Ability");
     JButton items = new JButton("Items");
-    public Display(Player p, Environment e, Party team)
+    public Display(Player p, Environment e)
     {
+        active = p;
+        actEnv = e;
+        screen = new JFrame("Fight Game");
+        panel = new Canvas(active, actEnv, this);
+        screen.add(panel);
         for(int i = 0; i < p.getAbilities().size(); i++)
         {
-            JButton btn = new JButton(p.getAbilities().get(i).getName());
+            JButton btn = p.getAbilities().get(i).getTrigger();
             groupAb.add(btn);
             abBar.add(btn);
-            btn.addActionListener(new ButtonListener());
+            btn.addActionListener(new ButtonListener(p, e));
         }
 
         if(p.isHealer())
@@ -44,7 +51,7 @@ public class Display extends JFrame
                 JButton btn = new JButton(e.getPlayers().get(i).getName());
                 groupAt.add(btn);
                 atBar.add(btn);
-                btn.addActionListener(new ButtonListener());
+                btn.addActionListener(new AuxiliaryButtonListener(p, e));
             }
         }
         else
@@ -54,7 +61,7 @@ public class Display extends JFrame
                 JButton btn = new JButton(e.getEnemies().get(i).getName());
                 groupAt.add(btn);
                 atBar.add(btn);
-                btn.addActionListener(new ButtonListener());
+                btn.addActionListener(new AuxiliaryButtonListener(p, e));
             }
         }
         JButton all = new JButton("All");
@@ -63,16 +70,15 @@ public class Display extends JFrame
 
     public void run()
     {
+
         atBar.add(atMenu);
         abBar.add(abMenu);
         tarBar.add(tarMenu);
         allBar.add(allMenu);
-        attack.addActionListener(new ButtonListener());
-        useAb.addActionListener(new ButtonListener());
-        items.addActionListener(new ButtonListener());
+        attack.addActionListener(new ButtonListener(active, actEnv));
+        useAb.addActionListener(new ButtonListener(active, actEnv));
         screen.setLayout(null);
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        screen.add(panel);
         panel.add(tarBar, BorderLayout.SOUTH);
         screen.setContentPane(panel);
         screen.setPreferredSize(new Dimension(1600, 900));
@@ -81,31 +87,56 @@ public class Display extends JFrame
         screen.add(items);
         screen.add(atBar);
         screen.add(abBar);
-        screen.add(itBar);
         screen.add(tarBar);
         screen.add(allBar);
         atBar.setVisible(false);
         abBar.setVisible(false);
-        itBar.setVisible(false);
         tarBar.setVisible(false);
         allBar.setVisible(false);
         screen.setLocationRelativeTo(null);
         screen.setVisible(true);
         screen.pack();
     }
+    public static void main(String[] args)
+    {
+        Dragon dragon = new Dragon(2);
+        Fighter ulfeen= new Fighter("Ulfeen", new ActorClass(0),2, new BufferedImage(1,2,3));
+        ArrayList<Player> allPlayers = new ArrayList<Player>();
+        ArrayList<Enemy> allEnemies = new ArrayList<Enemy>();
+        allPlayers.add(ulfeen);
+        allEnemies.add(dragon);
+        boolean canMoveOn = false;
+        Environment demoEnv = new Environment(ulfeen, allPlayers, allEnemies);
+        demoEnv.addActor(ulfeen);
+        Display d = new Display(ulfeen, demoEnv);
+        while(ulfeen.getHealth() > 0 && dragon.getHealth() > 0)
+        {
+            d.run();
+            dragon.act(demoEnv);
+            System.out.println("x");
+            while(!demoEnv.hasRan){}
+        }
+    }
 }
 class Canvas extends JPanel
 {
-    private Rectangle scoopnum;
     BufferedImage image1;
     BufferedImage ulfeen;
-    BufferedImage image3;
-    public Canvas()
+    ArrayList<Actor> playerStats;
+    ArrayList<Actor> enemyStats;
+    Player player;
+    Environment e;
+    Display disp;
+    public Canvas(Player p, Environment env, Display d)
     {
-        scoopnum = new Rectangle(25, 25, 25, 25);
+        disp = d;
+        player = p;
+        e = env;
+        playerStats = env.getPlayers();
+        enemyStats = (ArrayList<Actor>)env.getEnemies();
         try
         {
-            image1 = ImageIO.read(new File("src//souptime.jpg"));
+            image1 = ImageIO.read(new File("src//mullen.jpg"));
             ulfeen = ImageIO.read(new File("src//betterulf.jpg"));
         } catch(IOException e)
         {
@@ -119,10 +150,21 @@ class Canvas extends JPanel
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g.setColor(Color.red);
-        g2d.fill(scoopnum);
-        this.getGraphics().drawImage(image1, 50,300,null);
-        this.getGraphics().drawImage(ulfeen, 1000,50,null);
+        int y = 800;
+
+            g.drawString(player.getName() + ": Health: " + Integer.toString(player.getHealth()) + "/" + Integer.toString(player.getMaxHealth()) + "| Mana: " + Integer.toString(player.currentMana) + "/" + Integer.toString(player.maxMana),1000, y );
+
+        y = 800;
+        for(Actor a: enemyStats)
+        {
+            g.drawString(a.getName() + ": Health: " + Integer.toString(a.getHealth()) + " / " + Integer.toString(a.getMaxHealth()) + "| Mana: " + Integer.toString(a.currentMana) + "/" + Integer.toString(a.maxMana),50, y);
+        }
+        g.drawImage(image1, 50,100,null);
+        g.drawImage(ulfeen, 1000,100,null);
+        this.repaint();
     }
 
 
+
 }
+
